858aa8ae8ae8f8fd8a0eb431651b27ff32a70b31
Yago Santos Gigeck <yago0901@gmail.com>
2022-08-22 00:26:22 -0300

    Primeiro ajuste tela Calendário

--- screens/Calendario.tsx
+++ screens/Calendario.tsx
@@ -1,14 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet, Alert } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View, } from '../components/Themed';
 
 export default function Calendario() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Calendário de Batalhas</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      
+      <View style={styles.calentotal}>
+        <View style={styles.calentop} >
+
+          <Text style={styles.dia} onPress={() => Alert.alert('Botão Dom precionado')}>
+            Dom       
+          </Text>
+          <Text style={styles.dia} onPress={() => Alert.alert('Botão Seg precionado')}>
+            Seg
+          </Text>
+          <Text style={styles.dia} onPress={() => Alert.alert('Botão Ter precionado')}>
+            Ter
+          </Text>
+          <Text style={styles.dia} onPress={() => Alert.alert('Botão Qua precionado')}>
+            Qua
+          </Text>
+          <Text style={styles.dia} onPress={() => Alert.alert('Botão Qui precionado')}>
+            Qui
+          </Text>
+          <Text style={styles.dia} onPress={() => Alert.alert('Botão Sex precionado')}>
+            Sex
+          </Text>
+          <Text style={styles.dia} onPress={() => Alert.alert('Botão Sab precionado')}>
+            Sab
+          </Text>
+        </View>
+        
+        <View style={styles.calenhead} >
+
+        </View>
+      </View>
     </View>
   );
 }
@@ -17,9 +46,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
+    marginTop: 20,
     fontSize: 20,
     fontWeight: 'bold',
   },
@@ -28,4 +57,27 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  calentop: {
+    flexDirection: "row",
+    height: '4%',
+    width: '80%',
+        
+  },
+  dia: {
+    flex: 1,
+    textAlign: 'center',
+
+  },
+  calentotal: {
+    borderWidth: 2,
+    borderColor: "#1b58e9",
+    borderRadius: 6,
+    height: '80%',
+    backgroundColor: 'transparent',
+  },
+  calenhead: {
+    backgroundColor: "#1b58e9",
+    flex: 1,   
+  },
+  
 });
